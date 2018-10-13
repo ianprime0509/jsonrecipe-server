c443bf8f042744317e2d0edb3ac290d701bff6c2
Ian Johnson <ianprime0509@gmail.com>
2018-10-12 22:16:59 -0400

    Add tests for Location JSON
    The serialize tests are failing for some reason.

--- src/main/java/com/ianprime0509/jsonrecipe/server/entities/WebLocation.java
+++ src/main/java/com/ianprime0509/jsonrecipe/server/entities/WebLocation.java
@@ -2,13 +2,17 @@ package com.ianprime0509.jsonrecipe.server.entities;
 
 import java.net.URL;
 import java.time.LocalDate;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 /**
  * A website or other source accessible online.
  */
 @Data
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class WebLocation implements Location {
   /**
    * The URL of the source material.
---
+++ src/test/java/com/ianprime0509/jsonrecipe/server/json/LocationJsonTest.java
@@ -0,0 +1,55 @@
+package com.ianprime0509.jsonrecipe.server.json;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import java.net.URL;
+import java.time.LocalDate;
+import com.ianprime0509.jsonrecipe.server.entities.Location;
+import com.ianprime0509.jsonrecipe.server.entities.WebLocation;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@JsonTest
+public class LocationJsonTest {
+  @Autowired
+  private JacksonTester<Location> json;
+
+  @Test
+  public void testSerialize_webLocationWithUrlOnly() throws Exception {
+    assertThat(json.write(new WebLocation(new URL("https://www.gnu.org"))))
+        .isEqualToJson("{\"url\": \"https://www.gnu.org\"}");;
+  }
+
+  @Test
+  public void testSerialize_completeWebLocation() throws Exception {
+    assertThat(json
+        .write(new WebLocation(new URL("https://www.duckduckgo.com"), LocalDate.of(2018, 10, 12))))
+            .isEqualToJson(
+                "{\"url\": \"https://www.duckduckgo.com\", \"retrievalDate\": \"2018-10-12\"}");
+  }
+
+  @Test
+  public void testDeserialize_webLocationWithUrlOnly() throws Exception {
+    assertThat(json.parse("{\"url\": \"https://www.gnu.org\"}"))
+        .isEqualTo(new WebLocation(new URL("https://www.gnu.org")));
+  }
+
+  @Test
+  public void testDeserialize_completeWebLocation() throws Exception {
+    assertThat(
+        json.parse("{\"url\": \"https://www.duckduckgo.com\", \"retrievalDate\": \"2018-10-12\"}"))
+            .isEqualTo(
+                new WebLocation(new URL("https://www.duckduckgo.com"), LocalDate.of(2018, 10, 12)));
+  }
+
+  @Test
+  public void testDeserialize_unknownLocationType_fails() throws Exception {
+    assertThatExceptionOfType(Exception.class)
+        .isThrownBy(() -> json.parse("\"location\": \"somewhere\""));
+  }
+}
